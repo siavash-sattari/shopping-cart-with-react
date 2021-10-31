cf40c7a27d99f6363a08094054ab005b0f95f442
siavash sattari <siavash.sattari.75@gmail.com>
2021-10-31 15:04:01 +0330

    Product Details Page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
+import {Route, Switch, Redirect} from "react-router";
 
 // Components
 import Store from "./components/Store";
+import ProductDetails from "./components/ProductDetails";
 
 // Context
 import ProductContextProvider from "./context/ProductContextProvider";
 
 function App() {
   return (
-    <>
-      <ProductContextProvider>
-        <Store />
-      </ProductContextProvider>
-    </>
+    <ProductContextProvider>
+      <Switch>
+        <Route path="/products/:id" component={ProductDetails} />
+        <Route path="/products" component={Store} />
+        <Redirect to="/products" />
+      </Switch>
+    </ProductContextProvider>
   );
 }
 
--- src/components/shared/Product.js
+++ src/components/shared/Product.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 // Functions
 import {shorten} from "../helper/functions";
@@ -10,7 +11,7 @@ function Product({productData}) {
       <h3>{shorten(productData.title)}</h3>
       <p>{productData.price}</p>
       <div>
-        <a href="">Details</a>
+        <Link to={`/products/${productData.id}`}>Details</Link>
         <div>
           <button>Add To Cart</button>
         </div>
